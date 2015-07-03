6eb91730c56177c51481442ec2168bac909ce124
dstad <djs748@gmail.com>
2015-07-03 10:25:43 -0700

    i2c test

--- Arduino/i2c_practice.ino/i2c_practice.ino.ino
+++ Arduino/i2c_practice.ino/i2c_practice.ino.ino
@@ -1,15 +1,15 @@
 #include <TinyWireS.h>
 
 #define I2C_SLAVE_ADDRESS  0x04
-int led = 1;
+int button = 3;
 int i = 0;
 //#define data [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 void setup() {
   TinyWireS.begin(I2C_SLAVE_ADDRESS);
   TinyWireS.onRequest(requestEvent);
-  pinMode(led, OUTPUT);                     // initialize I2C lib
-  digitalWrite(led, HIGH);
+  pinMode(button, INPUT);                     // initialize I2C lib
+  digitalWrite(button, HIGH);
 }
 
 void loop() {
@@ -18,10 +18,9 @@ void loop() {
 
 void requestEvent()
 {
-  TinyWireS.send(i);
-  i++;
- 
+  if(digitalRead(button == LOW))
+  {
+    TinyWireS.send(i);
+  }
 }
-  
-
 
